be9812b16bc35688c47d448655afcec361a4ce0f
MALARVIZHI M <Malarm2004@gmail.com>
2024-01-31 18:47:38 +0530

    created basic structure

--- Tasktracker/src/App.jsx
+++ Tasktracker/src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/Header'
 import "./App.css"
 import Footer from './components/Footer'
 import Note from './components/Note'
+import CreateArea from './components/CreateArea'
 
 const App = () => {
   return (
     <div>
       <Header/>
-      <Note/>
+      <CreateArea/>
+      <Note key={1} title="Note title" content="Note Content"/>
       <Footer/>
     </div>
   )
